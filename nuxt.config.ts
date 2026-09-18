8f61280c78c90b89d7c2511bc0514b0164e953ba
Jögur <egorfdb@gmail.com>
2026-09-18 10:05:10 +0300

    feat: add Tauri desktop shell (#14)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
+  ssr: false,
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
+  vite: {
+    server: {
+      watch: {
+        ignored: /[/\\]src-tauri[/\\]target[/\\]/,
+      },
+    },
+  },
   typescript: {
     strict: true,
   },
