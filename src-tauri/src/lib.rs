d23df69107636d8a7d53dbe4bdee137dbd7767b8
Jögur <egorfdb@gmail.com>
2026-09-19 00:40:54 +0300

    feat: add SQLite music library storage (#30)
    Co-authored-by: Egor <e.trutchenkov@garlyn.ru>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,14 @@
 mod audio_metadata;
+mod music_library_storage;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
-            audio_metadata::read_audio_metadata
+            audio_metadata::read_audio_metadata,
+            music_library_storage::add_library_track,
+            music_library_storage::list_library_tracks,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
